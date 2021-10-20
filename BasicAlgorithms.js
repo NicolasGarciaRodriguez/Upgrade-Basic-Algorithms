3d59ff8dfceed600848856240ae0b0f338b1c053
NicoGarcia <nicogarcia.ro97@gmail.com>
2021-10-20 15:32:38 +0200

    Operadores

--- BasicAlgorithms.js
+++ BasicAlgorithms.js
@@ -1,47 +1,80 @@
 // 1. Variables
 
-const myFavouriteHero = "Hulk"
-const x = 50
-const h = 5
-const y = 10
-const z = h + y
+// const myFavouriteHero = "Hulk"
+// const x = 50
+// const h = 5
+// const y = 10
+// const z = h + y
 
 
-// 2. Variables avanzadas
+// // 2. Variables avanzadas
 
-const character = {name: 'Jack Sparrow', age: 10};
+// const character = {name: 'Jack Sparrow', age: 10};
 
-character["age"] = 25
+// character["age"] = 25
 
-// --------------------------------
+// // --------------------------------
 
-const firstName = 'Jon'; 
-const lastName = 'Snow'; 
-const age = 24; 
+// const firstName = 'Jon'; 
+// const lastName = 'Snow'; 
+// const age = 24; 
 
-const frase = `Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos`
+// const frase = `Soy ${firstName} ${lastName}, tengo ${age} años y me gustan los lobos`
 
-console.log(frase)
+// console.log(frase)
+
+// // ------------------------------------
+
+// const toy1 = {name: 'Buss myYear', price: 19};
+// const toy2 = {name: 'Rallo mcKing', price: 29};
+
+// console.log(toy1["price"] + toy2["price"])
+
+// // -----------------------------------
+
+// let globalBasePrice = 10000;
+// const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
+// const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+// globalBasePrice = 25000
+
+// car1["finalPrice"] = 75000
+// car2["finalPrice"] = 95000
+
+
+
+// 3. Operadores
+
+// let num1 = 10
+// let num2 = 5
+// let resultado = num1 * num2
+// alert(resultado)
+
+// // -----------------------------
+
+// let num3 = 10
+// let num4 = 2
+// let resultado2 = num3 / num4
+// alert(resultado2)
+
+// // ---------------------------------
+
+// let num5 = 15
+// let num6 = 9
+// let resto = num5 % num6
+// alert(resto)
 
 // ------------------------------------
 
-const toy1 = {name: 'Buss myYear', price: 19};
-const toy2 = {name: 'Rallo mcKing', price: 29};
+// y = 10
+// z = 5
+// x = z + y
+// console.log(x)
 
-console.log(toy1["price"] + toy2["price"])
+// ------------------------------------
 
-// -----------------------------------
-
-let globalBasePrice = 10000;
-const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
-const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
-
-globalBasePrice = 25000
-
-car1["finalPrice"] = 75000
-car2["finalPrice"] = 95000
-
-console.log(car1)
-console.log(car2)
-
+y = 10
+z = 5
+x = 10 * 5
+console.log(x)
 
