f539999f1acc420e30c4e18eb15e33ff8f9164b8
NicoGarcia <nicogarcia.ro97@gmail.com>
2021-10-20 15:11:24 +0200

    Variables avanzadas

--- BasicAlgorithms.js
+++ BasicAlgorithms.js
@@ -35,3 +35,13 @@ console.log(toy1["price"] + toy2["price"])
 let globalBasePrice = 10000;
 const car1 = {name: 'BMW m&m', basePrice: 50000, finalPrice: 60000};
 const car2 = {name: 'Chevrolet Corbina', basePrice: 70000, finalPrice: 80000};
+
+globalBasePrice = 25000
+
+car1["finalPrice"] = 75000
+car2["finalPrice"] = 95000
+
+console.log(car1)
+console.log(car2)
+
+
